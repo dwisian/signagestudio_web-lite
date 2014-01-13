f1fd58864b9f071ea3ae320644436b65b84c8a30
Sean <slevy@mediasignage.com>
2014-01-13 13:11:09 -0800

    Bootstrap responsive layout III

--- studiolite-bs.ts
+++ studiolite-bs.ts
@@ -8,6 +8,7 @@
 /// <reference path="./_view/AnotherView.ts" />
 /// <reference path="./AppRouter.ts" />
 
+var wB = 0;
 $(function () {
     var app_router = new AppRouter();
     Backbone.history.start();
@@ -17,32 +18,30 @@ $(function () {
     // $('#app').animate({opacity: 1},1500);
 
     $('#closePanel').click(function () {
-        $('#propPanel').fadeOut('50').animate({
-            width: '0%'
-        }, 1, function () {
-            $('#mainPanelWrap').animate({
-                width: '100%'
-            }, 100)
+        $('#propPanel').fadeOut(function () {
+            $('#propPanel').addClass('hidden-sm hidden-md');
+            $('#mainPanelWrap').removeClass('col-sm-9 col-md-9');
+            $('#mainPanelWrap').addClass('col-md-12');
         });
     });
 
-
     $('#openPanel').click(function () {
+        $('#mainPanelWrap').addClass('col-sm-9 col-md-9');
+        setTimeout(function(){
+            $('#mainPanelWrap').removeClass('col-md-12');
+            $('#propPanel').children().hide();
+            $('#propPanel').removeClass('hidden-sm hidden-md');
+            $('#propPanel').children().fadeIn();
+        },500)
+
+        return
 
         var w:number = parseFloat($('body').css('width').replace('px', ''));
         if (w <= 768) {
-            $('#closePanel').trigger('click');
+            // $('#closePanel').trigger('click');
             $('#myModal').modal('show');
             return false;
         }
-
-        $('#mainPanelWrap').animate({
-            width: '75%'
-        }, 50, function () {
-            $('#propPanel').animate({
-                width: '25%'
-            }, 100).fadeIn('10');
-        });
 
         return false;
 
@@ -58,32 +57,46 @@ $(function () {
 
     $(window).resize(function () {
 
+        $('#mainPanelWrap').removeClass('animateDivs');
+        $('#propPanel').removeClass('animateDivs');
+
         var h:number = <any>$('body').css('height').replace('px', '');
         var w:number = <any>($('body').css('width').replace('px', ''));
 
         h = h - 115;
 
-        if (w >= 1200) {
-            console.log($('#propPanel').width());
-            // alert($('#propPanel').width());
-            if ($('#propPanel').width() < 50)
-                $('#openPanel').trigger('click');
+        /*if (w >= 1200) {
+         console.log($('#propPanel').width());
+         // alert($('#propPanel').width());
+         if ($('#propPanel').width() < 50)
+         $('#openPanel').trigger('click');
+         } */
 
-        }
-
+        // fixed footer
         if (w <= 768) {
             h = h + 65;
-            if ($('#propPanel').width() != 0) {
-                console.log('Prop w: ' + $('#propPanel').width());
-                $('#propPanel').fadeOut(1).animate({
-                    width: '0%'
-                }, 1, function () {
-                    $('#mainPanelWrap').animate({
-                        width: '100%'
-                    }, 1)
-                });
-            }
         }
+
+        /*if (w >= 1200) {
+         $('#mainPanelWrap').width('66.68%');
+         $('#propPanel').width('16.66%');
+         $('#mainNav').width('16.66%');
+         } */
+
+        /*if (w <= 768) {
+         h = h + 65;
+         if ($('#propPanel').width() != 0) {
+         console.log('Prop w: ' + $('#propPanel').width());
+         $('#propPanel').fadeOut(1).animate({
+         width: '0%'
+         }, 1, function () {
+         $('#mainPanelWrap').animate({
+         width: '100%'
+         }, 1)
+         });
+         }
+         }
+         */
 
         console.log('resize ' + h);
 
